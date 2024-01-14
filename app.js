fca98fd4941dbc01c8e562caa78ae938e0e98c6c
delirium <evloev2021@bk.ru>
2024-01-14 17:06:53 +0300

    Операторы

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-let a = 1;
-console.log (a);
-a = 'test';
-console.log (a);
+// ариф операторы и присваивания
+const width = 10;
+const height = 5;
+const space = 10 * 5;
+const a = 2 ** 3 // 2 * 2 * 2 
+let age = 18;
+age += 2;// age = age + 2;
+age -= 3; // age = age - 3;
+age *= 2; // age = age * 2;
+age ++; // age = age + 1;
+age --; // age = age - 1;
+console.log(a); 
+console.log(age)
+// сравнения
+const vasyaAge = 20;
+console.log(age > vasyaAge)
